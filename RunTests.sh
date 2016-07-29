734a12a9131597cbafca14530c79adc6d2627050
zcobell <zachary.cobell@arcadis-us.com>
2016-07-29 15:16:47 -0600

    Adding first test suite

---
+++ RunTests.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+#...List of ADCIRC's test cases. Cases are named by their directory tree
+case_list=( adcirc/adcirc_quarterannular-2d          \
+            adcirc/adcirc_quarterannular-2d-parallel \
+            adcirc/adcirc_shinnecock_inlet           \
+            adcirc/adcirc_shinnecock_inlet-parallel  \
+            adcirc/adcirc_internal_overflow          \
+            adcirc/adcirc_internal_overflow-parallel \
+            adcirc/adcirc_apes                       \
+            adcirc/adcirc_apes-parallel              )
+
+#...Maximum Absoloute Error
+abserr=0.00001
+
+#...Maximum Relative Error
+relerr=0.00001
+
+#...Path to the executables
+adcirc_path=$1
+
+#...Object directories for each executable
+ODIR_adcirc=$adcirc_path/odir3
+ODIR_padcirc=$adcirc_path/odir4
+ODIR_adcprep=$adcirc_path/odir1
+
+#...Current home location
+TESTHOME=$(pwd)
+
+#...Sanity check on script arguments
+if [ $# -ne 1 ] ; then
+    echo "ERROR: Script requires 1 argument with folder containing adcirc.exe and adccmp.exe"
+    exit 1
+fi
+
+#...Ensure that a relative path is not supplied
+if [ "x${adcirc_path:0:1}" != "x/" ] ; then
+    echo "ERROR: You must provide an absoloute path."
+    exit 1
+fi
+
+#...Check if adcirc exists
+if [ ! -s $1/adcirc ] ; then
+    echo "ERROR: adcirc executable not found."
+    exit 1
+fi
+
+#...Check if adcprep exists
+if [ ! -s $1/adcprep ] ; then
+    echo "ERROR: adcprep executable not found."
+    exit 1
+fi
+
+#...Check if padcirc exists
+if [ ! -s $1/padcirc ] ; then
+    echo "ERROR: padcirc executable not found."
+    exit 1
+fi
+
+#...Check of adccmp exists
+if [ ! -s $1/adccmp ] ; then
+    echo "ERROR: adccmp executable not found."
+    exit 1
+fi
+
+#...Check for GCOV files
+if [ -s $ODIR_adcirc/adcirc.gcno ] ; then
+    coverage=1
+else
+    coverage=0
+fi
+
+#...Loop to run over all the test cases
+for CASE in ${case_list[@]}
+do
+
+    #...Proceed into the case directory
+    cd $CASE
+
+    #...Check for the generic run.sh script
+    if [ ! -s run.sh ]; then
+        echo "ERROR: Could not find run script for $CASE"
+        exit 1
+    fi
+    
+    #...Run the case and check the return status
+    ./run.sh $adcirc_path $abserr $relerr 2>/dev/null
+    if [ $? -ne 0 ] ; then
+        echo "ERROR: The case $CASE did not pass."
+        exit 1
+    fi
+
+    #...Back to testing home location
+    cd $TESTHOME
+done
+
+#...Check to see if we need to generate coverage report
+if [ "x$coverage" == "x1" ] ; then
+    echo "Generating ADCIRC Coverage Report..."
+    ./GenerateCoverageReport.sh ADCIRC $ODIR_adcirc
+    echo ""
+    echo "Generating ADCPREP Coverage Report..."
+    ./GenerateCoverageReport.sh ADCPREP $ODIR_adcprep
+    echo ""
+    echo "Generating PADCIRC Coverage Report..."
+    ./GenerateCoverageReport.sh PADCIRC $ODIR_padcirc
+fi
+    
+
+#...Exit with status zero if all tests have passed
+exit 0
---
+++ adcirc/adcirc_quarterannular-2d-parallel/run.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+case_name="quarter_annular-2d-parallel"
+
+#...Check on what is provided
+if [ $# -ne 3 ] ; then
+    echo "ERROR: Script requires 3 arguments!"
+    echo "    Argument 1: Folder containing adcirc and adccmp executables."
+    echo "    Argument 2: Maximum absoloute error"
+    echo "    Argument 3: Maximum relative error"
+    echo "Exiting with status 1, Failed."
+    exit 1
+fi
+
+#...Set variables
+exepath=$1
+abserr=$2
+relerr=$3
+
+#...Run the case
+echo ""
+echo "|---------------------------------------------|"
+echo "    TEST CASE: $case_name"
+echo ""
+echo -n "    Prepping case..."
+$exepath/adcprep --np 2 --partmesh >  adcprep.log
+$exepath/adcprep --np 2 --prepall  >> adcprep.log
+echo "done!"
+echo -n "    Runnning case..."
+mpirun -np 2 $exepath/padcirc > padcirc_log.txt
+exitstat=$?
+echo "Finished"
+echo "    PADCIRC Exit Code: $exitstat"
+if [ "x$exitstat" != "x0" ] ; then
+    echo "    ERROR: PADCIRC did not exit cleanly."
+    exit 1
+fi
+echo ""
+
+
+#...Run the comparison test
+echo -n "    Running comparison..."
+$exepath/adccmp control . ETA2 $abserr $relerr > wse_comparison.log
+$exepath/adccmp control . VV2  $abserr $relerr > vel_comparison.log
+nerror_wse=$(cat wse_comparison.log | grep failed | wc -l)
+nerror_vel=$(cat vel_comparison.log | grep failed | wc -l)
+echo "Finished"
+
+#...Check the number of failed steps
+if [ "x$nerror_wse" == "x0" -a "x$nerror_vel" == "x0" ] ; then
+    echo "    Test $case_name Passed!"
+    echo "|---------------------------------------------|"
+    echo ""
+    exit 0
+else
+    echo "    ERROR: Test $case_name Failed!"
+    echo ""
+    echo "    ERROR Summary"
+    echo "      WSE Errors: $nerror_wse"
+    echo "      VEL Errors: $nerror_vel"
+    echo "|---------------------------------------------|"
+    echo ""
+    exit 1
+fi
