437e776694faa22ad8cf0c4d2dde8d4b7e45cc4e
Zach Cobell <zachary.cobell@arcadis-us.com>
2018-04-19 22:49:11 -0600

    Updating mpirun command for root on test server

--- adcirc/adcirc_quarterannular-2d-parallel-writer/run.sh
+++ adcirc/adcirc_quarterannular-2d-parallel-writer/run.sh
@@ -35,7 +35,7 @@ else
 fi
 
 echo -n "    Runnning case..."
-mpirun -np $np $exepath/padcirc > padcirc_log.txt
+mpirun --allow-run-as-root -np $np $exepath/padcirc > padcirc_log.txt
 exitstat=$?
 echo "Finished"
 echo "    ADCIRC Exit Code: $exitstat"
