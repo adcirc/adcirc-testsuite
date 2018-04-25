4a7748a153eafe892b4c0fadf5a4c30bc3e2b223
Zach Cobell <zachary.cobell@arcadis-us.com>
2018-04-24 22:48:08 -0600

    Adding coverage report html from lcov
    Former-commit-id: 9a90a2c80027bd7725efdb6d0d5c254a7e316717

--- GenerateCoverageReport.sh
+++ GenerateCoverageReport.sh
@@ -1,56 +1,51 @@
 #!/bin/bash
 
-if [ $# != 2 ] ; then
-    echo "ERROR: Must include two arguments with executable name and directory to generate coverage report for"
+if [ $# != 1 ] ; then
+    echo "ERROR: Specify the build directory to generate the coverage report"
     exit 1
 fi
 
-#...Move into the source code object directory
-cd $2
-
-execLines=0
-totalLines=0
-
-gcovexe=$(which gcov)
-if [ "x$gcovexe" == "x" ] ; then
-    echo "ERROR: No gcov executable found"
+lcovexe=$(which lcov)
+if [ "x$lcovexe" == "x" ] ; then
+    echo "ERROR: No lcov executable found"
     exit 1
 fi
 
-header=$(printf "%55.55s" "$1 CODE COVERAGE REPORT")
-echo "|--------------------------------------------------------------------------------------|"
-echo "| $header                              |"
-echo "|--------------------------------------------------------------------------------------|"
-echo "|              FILE              | LINES IN FILE | LINES EXECUTED | PERCENT COVERAGE   |"
-echo "|--------------------------------------------------------------------------------------|"
-#...Loop over output files
-for OBJECT in *.o
+genhtmlexe=$(which genhtml)
+if [ "x$genhtmlexe" == "x" ] ; then
+    echo "ERROR: No generate html executable found"
+    exit 1
+fi
+
+basedir=$1/CMakeFiles
+dirs=("adccmp.dir adcirc.dir" "adcircResultsComparison.dir" "adcprep.dir" "adcswan.dir" \
+      "aswip.dir build13.dir" "buildstwave23.dir" "hot2asc.dir" "hstime.dir" "inflate.dir" \
+      "metis.dir" "mkdir.dir" "owi22.dir" "p15.dir" "padcirc.dir" "padcswan.dir" \ 
+      "templib_adcirc1.dir" "templib_adcswan1.dir" "templib_adcswan2.dir" \
+      "templib_adcswan3.dir" "templib_padcirc1.dir" "templib_padcirc2.dir" "templib_padcirc3.dir" \
+      "templib_padcswan1.dir" "templib_padcswan2.dir" "templib_padcswan3.dir" "templib_padcswan4.dir" \
+      "templib_padcswan5.dir" "templib_swan1parallel.dir" "templib_swan2parallel.dir" 
+      "templib_swan1serial.dir" "templib_swan2serial.dir")
+
+#...Generate the trace files
+mkdir -p adcirc.cov
+cd adcirc.cov 
+
+echo "Processing coverage files..."
+testString=""
+for DIR in ${dirs[@]}
 do
-    temp=$(gcov $OBJECT 2>/dev/null)
-    if [ "x$temp" != "x" ] ; then
-        file=$(basename $(echo $temp | cut -d\' -f2 | cut -d\' -f1))
-        percent=$(echo $temp | cut -d: -f2 | cut -d% -f1)
-        lines=$(echo $temp | cut -d% -f2 | cut -d" " -f3)
-       
-        execd=$(echo "$lines * $percent / 100" | bc)
-        execLines=$(echo "$execLines + $execd" | bc)
-        totalLines=$(echo "$totalLines + $lines" | bc)
-        
-        ps=$(printf "%17.17s" $percent)
-        le=$(printf "%14.14s" $execd)
-        ln=$(printf "%13.13s" $lines)
-        fl=$(printf "%30.30s" $file)
-        echo "| $fl | $ln | $le | $ps% |"
+    $lcovexe -t "adcircTestSuite" -o $DIR.info -c -d $basedir/$DIR > stat.txt 2>stat.txt
+    n=$(grep "skipping" stat.txt | wc -l)
+    if [ "x$n" == "x0" ] ; then
+        testString="$testString -a $DIR.info" 
     fi
 done
-echo "|--------------------------------------------------------------------------------------|"
-el=$(printf "%8.8s" $execLines)
-tl=$(printf "%8.8s" $totalLines)
-tc=$(echo "scale=4;($el/$tl)*100" | bc)
-tp=$(printf "%0.2f" $tc)
-tp=$(printf "%8.8s" $tp)
-echo "  Total Lines Executed: $el"
-echo "   Total Lines in Code: $tl"
-echo "Total percent coverage: $tp%"
+    
+#...Merge data in lcov 
+echo "Merging coverage files..."
+$lcovexe $testString -o adcirc_full.info >/dev/null 2>/dev/null 
 
-exit 0
+#...Generate the HTML
+echo "Generating webpage..."
+$genhtmlexe --legend --num-spaces 4 -f -o adcirc adcirc_full.info >/dev/null 2>/dev/null
