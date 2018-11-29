d80c9b6d29ccbad6e3f8c6f480c4166e7a789543
Jason Fleming <jason.fleming@seahorsecoastal.com>
2018-11-29 17:07:09 -0500

    Fixed stray -t (typo) in command line options to adcircResultsComparison.
    Former-commit-id: a376070b81b40b5a11574d2ac5de777c9c046421

--- adcirc/adcirc_apes/run.sh
+++ adcirc/adcirc_apes/run.sh
@@ -47,7 +47,7 @@ do
     if [[ ${files[$i]} = "maxvel.63" || ${files[$i]} = "maxele.63" || ${files[$i]} = "maxwvel.63" || ${files[$i]} = "minpr.63" ]]; then
        CLOPTIONS="$CLOPTIONS --minmax"
     fi
-    $exepath/adcircResultsComparison -t $CLOPTIONS -f1 ${files[$i]} -f2 control/${files[$i]} >> comparison.log 2>>comparison.log
+    $exepath/adcircResultsComparison $CLOPTIONS -f1 ${files[$i]} -f2 control/${files[$i]} >> comparison.log 2>>comparison.log
     error[$i]=$?
 done
 echo "Finished"
